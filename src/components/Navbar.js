2f3ee3f968d5845d445ec023e9720518c29c4a7a
Federico Silvera <federiconicolas007@gmail.com>
2023-11-10 22:13:41 -0300

    project-initial-commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "../App.css"
+export const Navbar = () => {
+  return (
+    <>
+    
+    <div className='flex-container'>
+        <h1 className='icon'>Project</h1>
+            <ul>
+                <li>
+                Home
+                </li>
+                <li>
+                Contact
+                </li>
+                <li>
+                Sign in
+                </li>
+                <li>
+                Prices
+            </li>
+          </ul>          
+    </div>
+    </>
+  )
+}
