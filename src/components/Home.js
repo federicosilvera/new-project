c04de657212d58e3e77b7f010b36c8ffc08fccec
Federico Silvera <federiconicolas007@gmail.com>
2023-11-12 11:27:15 -0300

    recuadro de

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import "./Home.css";
 
 
 export const Home = ({people}) => {
   return (
     <>
-    <div>
+    <div >
         {people.map(peoples=>
-            <div key={peoples.id}>
-                <p>Name: {peoples.username} LastName: {peoples.lastname}</p>
+            <div className='main-container' key={peoples.id}>
+              <div className='img-container'>{peoples.perfil} </div>
+                <p className='names-container'>Name: {peoples.username} LastName: {peoples.lastname}</p>
                 
             </div>
         
